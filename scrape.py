30c00205459909ef5f42df411d558e29719005ef
Alec Barran <alexanderbarran@gmail.com>
2024-11-02 18:05:29 -0700

    add year iterator to scrape seasons

--- scrape.py
+++ scrape.py
@@ -35,10 +35,19 @@ def fix_missing_ids(pbp: pd.DataFrame, shifts: pd.DataFrame) -> pd.DataFrame:
     return pbp
 
 if __name__ == "__main__":
-    scraped = hockey_scraper.scrape_date_range('2017-12-08', '2017-12-08', True, data_format="pandas")  # includes four players with missing IDs for testing
+    for y in range(2007,2024):
+        try:
+            scraped = hockey_scraper.scrape_seasons([y], True, data_format="pandas")
+            # scraped = hockey_scraper.scrape_date_range('2017-12-08', '2017-12-08', True, data_format="pandas")  # includes four players with missing IDs for testing
 
-    shifts = scraped["shifts"]
-    pbp = fix_missing_ids(scraped["pbp"], shifts)
+            shifts = scraped["shifts"]
+            pbp = scraped["pbp"]
+            # pbp = fix_missing_ids(pbp, shifts)
 
-    shifts.to_parquet("shift.parquet")
-    pbp.to_parquet("game.parquet")
+            shifts['Period'] = pd.to_numeric(shifts['Period'], errors='coerce')
+            pbp['Period'] = pd.to_numeric(pbp['Period'], errors='coerce')
+
+            shifts.to_parquet(f"shift_{y}.parquet")
+            pbp.to_parquet(f"game_{y}.parquet")
+        except TimeoutError:
+            continue
