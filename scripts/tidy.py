88272d0a0d12cc8723ba471aed1932d9bdd19ed2
Alex <alexander.hagood@wsu.edu>
2024-11-27 15:35:46 -0800

    add season to pbp

--- scripts/tidy.py
+++ scripts/tidy.py
@@ -9,6 +9,17 @@ pbp = pd.read_parquet('./data/pbp_combined.parquet')
 
 pbp['Date'] = pd.to_datetime(pbp['Date'])
 
+season_list = []
+season = 2007
+comparison_date = datetime.strptime('2008-08-31', '%Y-%m-%d')
+for row in pbp.itertuples(index=True):
+    if row.Date > comparison_date:
+        season += 1
+        comparison_date += timedelta(days=365)
+
+    season_list.append(season)
+pbp['Season'] = season_list
+
 games = pbp[['Game_Id', 'Date', 'Home_Team', 'Home_Coach', 'Away_Team', 'Away_Coach', 'Event', 'Period', "Home_Score", "Away_Score"]]
 games = games[games["Event"] == "GEND"]
 games.drop(columns=['Event'], inplace=True)
@@ -35,17 +46,6 @@ pbp.drop(columns=nameColumns + ["Home_Goalie", "Away_Goalie"], inplace=True)
 pbp.columns = [re.sub(r'(away|home)Player(\d)_id', lambda m: f"{m.group(1).title()}_p{m.group(2)}", col) for col in pbp.columns]
 pbp.columns = [re.sub(r'p(\d)_(ID|name)', lambda m: f"p{m.group(1)}_{m.group(2).title()}", col) for col in pbp.columns]
 
-season_list = []
-season = 2007
-comparison_date = datetime.strptime('2008-08-31', '%Y-%m-%d')
-for row in games.itertuples(index=True):
-    if row.Date > comparison_date:
-        season += 1
-        comparison_date += timedelta(days=365)
-
-    season_list.append(season)
-
-games['Season'] = season_list
 
 games.to_parquet('./data/games.parquet', index=False)
 playerframe.to_parquet('./data/players.parquet', index=False)
