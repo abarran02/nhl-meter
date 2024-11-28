8d6177345492562ec3ed714babd4d635de159339
Alex <alexander.hagood@wsu.edu>
2024-11-27 20:01:04 -0800

    oh yeah

---
+++ main.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import numpy as np
+import math
+
+class NHL_METER:
+    def __init__(self):
+        self.games = pd.read_parquet('./data/games.parquet')
+        self.pbp = pd.read_parquet('./data/pbp_reduced.parquet')
+        self.games["Game_Id"] = self.games["Game_Id"].astype("Int64")
+        pass
+
+    def get_pbp(self, game_id, season):
+        return self.pbp[(self.pbp['Game_Id'] == game_id) & (self.pbp['Season'] == season)]
+    
+    def add_won_column(self, games, pbp):
+        pbp_w = pd.merge(pbp, games[["Season", "Game_Id", "Away_Score", "Home_Score"]], how='left', on=['Game_Id', 'Season'], suffixes = ["", "_Final"])
+        pbp_w["Home_Won"] = pbp_w["Away_Score_Final"] < pbp_w["Home_Score_Final"]
+        pbp_w.drop(columns=["Away_Score_Final", "Home_Score_Final"], inplace=True)
+        return pbp_w
+    
+    # create some random winprob for testing yknow
+    def random_winprob(self, pbp):
+        pbp["Win_Prob"] = np.random.rand(pbp.shape[0])
+        return pbp
+    
+    def bin_winprob(self, pbp, binCount=10):
+        bins = np.linspace(0, 1, binCount)
+        digitized = np.digitize(pbp["Win_Prob"], bins)
+        pbp["bin"] = digitized
+        binned = []
+        for x in range(binCount):
+            binned.append(pbp[pbp["bin"] == x])
+        return binned
+    
+    # returns [(actual, predicted)]
+    # pass in result from bin_winprob
+    def analyze_accuracy(self, binned):
+        trueWin = []
+        for i, df in enumerate(binned):
+            winProp = df["Home_Won"].mean()
+            if (math.isnan(winProp)):
+                winProp = 0.0
+            est = (i + .5) / len(binned)
+            #print(f"Estimated: {est}, Actual: {winProp}")
+            trueWin.append((est, winProp))
+        return trueWin
+    
+    def least_squares(self, analyzed):
+        return sum([(x[0]-x[1])**2 for x in analyzed])
+
+    def log_loss(self, analyzed):
+        total = 0
+        for index, row in analyzed.iterrows():
+            actual = row["Home_Won"]
+            predicted = row["Win_Prob"]
+            total += (actual * math.log(predicted) + (1 - actual) * math.log(1 - predicted))
+        res = -1 / len(analyzed)
+        return res * total
+    
+    # 
+    def backtest(self, plays, loss_func):
+        binned_plays = self.bin_winprob(plays)
+        analyzed = self.analyze_accuracy(binned_plays)
+        return loss_func(analyzed)
+    
