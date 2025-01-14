a96a779f9c11ecce9cb4606017c7112ba4562356
Alec Barran <alexanderbarran@gmail.com>
2025-01-14 15:38:57 -0800

    Overtime Again (#10)
    * use sliding window with overtime LSTM and combine regular season and playoff models
    
    * include regular season goals in overtime data
    
    * clean up data loading and rearrange prediction script

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import json
+from pathlib import Path
+
 import numpy as np
 import pandas as pd
 import plotly.graph_objects as go
@@ -5,16 +8,25 @@ from dash import Dash, dcc, html
 from dash.dependencies import Input, Output, State
 from keras.models import load_model
 
+from dev import meter
 from dev.graphing import gutils
 
-games = pd.read_parquet("data/games.parquet")
-slices = pd.read_parquet("data/time_slices.parquet")
-regular_ot_pbp = pd.read_parquet("data/regular_ot_pbp.parquet")
-playoff_ot_pbp = pd.read_parquet("data/playoff_ot_pbp.parquet")
+current_file_path = Path(__file__).resolve()
+data_path = current_file_path.parent / "data"
+model_path = current_file_path.parent / "dev" / "models"
 
-model_regulation = load_model("dev/models/meter_lstm16d2.keras")
-model_regular_ot = load_model("dev/models/meter_reg_ot_lstm16.keras")
-model_playoff_ot = load_model("dev/models/meter_ply_ot_lstm16.keras")
+games = pd.read_parquet(data_path / "games.parquet")
+slices = pd.read_parquet(data_path / "time_slices.parquet")
+ot_pbp = pd.concat([
+    pd.read_parquet(data_path / "regular_ot_pbp.parquet"),
+    pd.read_parquet(data_path / "playoff_ot_pbp.parquet")
+])
+
+with open(model_path / "one_hot_columns.json", 'r') as f:
+    one_hot_columns = json.load(f)
+
+model_regulation = load_model(model_path / "meter_lstm16d2.keras")
+model_overtime = load_model(model_path / "meter_ot_lstm16d1.keras")
 
 teams = games["Home_Team"].unique()
 teams.sort()
@@ -86,39 +98,6 @@ def update_game_dropdown(home, away):
         "value": f'{g["Game_Id"]}.{g["Season"]}'
     } for idx, g in games_reduced.iterrows()]
 
-def predict_regulation(game: int, season: int) -> tuple[pd.Series, np.ndarray]:
-    selected_game = slices[(slices["game"] == game) & (slices["season"] == season)]
-
-    X = selected_game.drop(columns=["winner", "game", "season"])
-
-    probabilities = model_regulation.predict(X)
-
-    # convert from normalized 1 to 0
-    time_elapsed = 3600 - (selected_game["time_remaining"] * 3600)
-
-    return (time_elapsed, probabilities.flatten())
-
-def predict_overtime(game: int,
-                     season: int,
-                     playoff: bool) -> tuple[pd.Series, np.ndarray]:
-    if playoff:
-        mask = (playoff_ot_pbp["game"] == game) & (playoff_ot_pbp["season"] == season)
-        selected_game = playoff_ot_pbp[mask]
-        model = model_playoff_ot
-        time_elapsed = selected_game["seconds_elapsed"]
-    else:
-        mask = (regular_ot_pbp["game"] == game) & (regular_ot_pbp["season"] == season)
-        selected_game = regular_ot_pbp[mask]
-        model = model_regular_ot
-        time_elapsed = 300 - selected_game["time_remaining"]
-
-    X = selected_game.drop(["winner", "season", "game"], axis=1)
-    X_encoded = pd.get_dummies(X, columns=["event", "team", "event_zone", "home_zone", "strength"])
-
-    probabilities = model.predict(X_encoded)
-
-    return (3600 + time_elapsed, probabilities.flatten())
-
 @app.callback(
     Output("probability-graph", "figure"),
     [Input("home-dropdown", "value"),
@@ -127,6 +106,7 @@ def predict_overtime(game: int,
 )
 def update_figure(home, away, game_season):
     if not game_season:
+        # prevent drawing incomplete dropdown selection
         return go.Figure()
 
     game, season = [int(x) for x in game_season.split('.')]
@@ -139,13 +119,13 @@ def update_figure(home, away, game_season):
         idx += 1
         away_name_color = gutils.team_name_color(away, idx)
 
-    time_elapsed, probabilities = predict_regulation(game, season)
+    time_elapsed, probabilities = meter.predict_regulation(game, season, slices, model_regulation)
 
     # handle overtime games
     mask = (games["Game_Id"] == game) & (games["Season"] == season)
     game_data = games[mask].iloc[0]
     if game_data["Period"] > 3:
-        time_elapsed_ot, probabilities_ot = predict_overtime(game, season, game_data["Playoff"])
+        time_elapsed_ot, probabilities_ot = meter.predict_overtime(game, season, ot_pbp, model_overtime, one_hot_columns)
 
         # remove last data point of regulation to prevent overlap
         time_elapsed = pd.concat([time_elapsed[:-1], time_elapsed_ot])
---
+++ dev/meter.py
@@ -0,0 +1,138 @@
+import pandas as pd
+import numpy as np
+
+
+def plays_preceding_goals(pbp: pd.DataFrame, window_size: int, include_goal: bool = True, convert_winner: bool = False) -> pd.DataFrame:
+    """
+    Reduces play-by-play DataFrame to plays preceding each goal.
+
+    Args:
+        pbp (pd.DataFrame): NHL play-by-play data.
+        window_size (int): Number of plays preceding goal to include.
+        include_goal (bool, optional): Whether to include the GOAL play. Defaults to True.
+        convert_winner (bool, optional): For regular season games, converts the `winner`
+            column to the team of each goal scored. Also converts game IDs to random,
+            negative values so that each goal is treated as a separate game. Defaults to False.
+
+    Returns:
+        pd.DataFrame: Reduced DataFrame.
+    """
+
+    goal_indices = pbp.index[pbp['event'] == 'GOAL']
+
+    windows = []
+    targets = []
+
+    result_indices = []
+    for idx in goal_indices:
+        # may include GOAL row for use with sliding window
+        end = idx + 1 if include_goal else idx
+        window = range(max(idx - window_size, 0), end)
+
+        if convert_winner:
+            windows.append(window)
+            targets.append(pbp.iloc[window[-1]]["team"])
+
+        result_indices.extend(window)
+
+    if not convert_winner:
+        return pbp.loc[result_indices]
+    else:
+        # prevent changing original dataframe
+        conv = pbp.copy()
+
+        # change winner column in each window to goal scorer
+        # convert game ID to arbitrary value
+        winners = np.array(pbp["winner"].values)
+        ids = np.array(pbp["game"].values)
+
+        # use negative index as "game ID" since real IDs are positive
+        for idx, (rng, value) in enumerate(zip(windows, targets)):
+            winners[list(rng)] = value
+            ids[list(rng)] = -idx
+
+        # set DataFrame columns
+        conv["winner"] = winners
+        conv["game"] = ids
+
+        return conv.loc[result_indices]
+
+def sliding_window_game_pbp(pbp: pd.DataFrame, window_size: int) -> tuple[np.ndarray, np.ndarray]:
+    """Perform sliding window preprocessing on games' play-by-play data.
+
+    Args:
+        pbp (pd.DataFrame): NHL play-by-play data.
+        window_size (int): Size of the sliding window.
+
+    Returns:
+        tuple[np.ndarray, np.ndarray]: Arrays of play-by-play windows and targets (winners), respectively.
+    """
+    grouped = pbp.groupby(["season", "game"])  # unnecessary for individual games but don't want to cross over
+    windows = []
+    targets = []
+
+    for group_name, group in grouped:
+        temp_window = []
+        target = group["winner"].iloc[0]  # same for all in group
+
+        for row in group.drop("winner", axis=1).itertuples(index=False):
+            feature_values = list(row)[2:]  # skip season and game columns
+            temp_window.append(feature_values)
+
+            if len(temp_window) == window_size:
+                windows.append(temp_window.copy())
+                targets.append(target)
+                temp_window.pop(0)
+
+    return np.array(windows), np.array(targets)
+
+def predict_regulation(game: int, season: int, slices: pd.DataFrame, model) -> tuple[pd.Series, np.ndarray]:
+    """Predict regulation win probabilities for given NHL game.
+
+    Args:
+        game (int): NHL Game ID
+        season (int): NHL season
+        ot_pbp (pd.DataFrame): regulation time slice data
+        model (Model): keras Model
+
+    Returns:
+        tuple[pd.Series, np.ndarray]: Time series and win probability
+    """
+    selected_game = slices[(slices["game"] == game) & (slices["season"] == season)]
+
+    X = selected_game.drop(columns=["winner", "game", "season"])
+
+    probabilities = model.predict(X)
+
+    # convert from normalized 1 to 0
+    time_elapsed = 3600 - (selected_game["time_remaining"] * 3600)
+
+    return (time_elapsed, probabilities.flatten())
+
+def predict_overtime(game: int, season: int, ot_pbp: pd.DataFrame, model, one_hot_columns: list[str]) -> tuple[pd.Series, np.ndarray]:
+    """Predict overtime win probabilities for given NHL game.
+
+    Args:
+        game (int): NHL Game ID
+        season (int): NHL season
+        ot_pbp (pd.DataFrame): overtime play-by-play data
+        model (Model): keras Model
+        one_hot_columns (list[str]): one-hot encoding columns from training
+
+    Returns:
+        tuple[pd.Series, np.ndarray]: Time series and win probability
+    """
+
+    mask = (ot_pbp["game"] == game) & (ot_pbp["season"] == season)
+    selected_game = ot_pbp[mask]
+
+    # one-hot encode play-by-play and match columns to training data
+    X = selected_game.drop(["seconds_elapsed"], axis=1)
+    X_encoded = pd.get_dummies(X, columns=["event", "team", "event_zone", "home_zone", "strength"])
+    X_encoded = X_encoded.reindex(columns=one_hot_columns, fill_value=False)
+
+    windows, targets = sliding_window_game_pbp(X_encoded, 3)
+
+    probabilities = model.predict(windows)
+
+    return (3600 + selected_game["seconds_elapsed"], probabilities.flatten())
