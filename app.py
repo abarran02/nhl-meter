18957baa9b1fe3d0425477b5b0acfc3b58a9316c
Alec Barran <alexanderbarran@gmail.com>
2025-01-19 17:23:52 -0800

    add icons and links for GitHub and LinkedIn

--- app.py
+++ app.py
@@ -2,7 +2,14 @@ from dash import Dash
 from layout import app_layout
 from callbacks import register_callbacks
 
-app = Dash(title="NHL Meter")
+external_stylesheets = [
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css'
+]
+
+app = Dash(
+    title="NHL Meter",
+    external_stylesheets=external_stylesheets
+)
 app.layout = app_layout
 
 register_callbacks(app)
--- layout.py
+++ layout.py
@@ -5,33 +5,51 @@ games = pd.read_parquet("data/games.parquet")
 teams = games["Home_Team"].unique()
 teams.sort()
 
+linkedin_url = "https://www.linkedin.com/in/abarran/"
+github_url = "https://github.com/abarran02/nhl-meter"
+
+footer_style = {
+    "text-align": "center",
+    "display": "flex",
+    "align-items": "right",
+    "justify-content": "right",
+    "gap": "10px"
+}
+
 app_layout = html.Div([
-    html.Div(style={"flex": "10%"}),
+    html.Header([
+        html.A(html.I(className="fab fa-linkedin fa-lg"), href=linkedin_url, target="_blank"),
+        html.A(html.I(className="fab fa-github fa-lg"), href=github_url, target="_blank")
+    ], style=footer_style),
+
     html.Div([
-        html.H3("Teams and Games"),
+        html.Div(style={"flex": "10%"}),
         html.Div([
-            html.Label("Home Team"),
-            dcc.Dropdown(
-                options=[{"label": team, "value": team} for team in teams],
-                value=teams[0],
-                id="home-dropdown"
-            ),
-        ], className="dropdown-container"),
+            html.H3("Teams and Games"),
+            html.Div([
+                html.Label("Home Team"),
+                dcc.Dropdown(
+                    options=[{"label": team, "value": team} for team in teams],
+                    value=teams[0],
+                    id="home-dropdown"
+                ),
+            ], className="dropdown-container"),
+            html.Div([
+                html.Label("Away Team"),
+                dcc.Dropdown(
+                    options=[{"label": team, "value": team} for team in teams],
+                    id="away-dropdown"
+                ),
+            ], className="dropdown-container"),
+            html.Button("Switch", id="switch-button"),
+            html.Div([
+                html.Label("Games"),
+                dcc.Dropdown(id="game-dropdown"),
+            ], className="dropdown-container", style={"margin-top": "20px"})
+        ], style={"flex": "15%"}),
         html.Div([
-            html.Label("Away Team"),
-            dcc.Dropdown(
-                options=[{"label": team, "value": team} for team in teams],
-                id="away-dropdown"
-            ),
-        ], className="dropdown-container"),
-        html.Button("Switch", id="switch-button"),
-        html.Div([
-            html.Label("Games"),
-            dcc.Dropdown(id="game-dropdown"),
-        ], className="dropdown-container", style={"margin-top": "20px"})
-    ], style={"flex": "15%"}),
-    html.Div([
-        dcc.Graph(id="probability-graph")
-    ], style={"flex": "auto"}),
-    html.Div(style={"flex": "10%"}),
-], style={"display": "flex"})
+            dcc.Graph(id="probability-graph")
+        ], style={"flex": "auto"}),
+        html.Div(style={"flex": "10%"}),
+    ], style={"display": "flex", "flex-direction": "row"})
+])
